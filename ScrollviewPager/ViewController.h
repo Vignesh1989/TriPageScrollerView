c37bfdb0e4453a5b7e722ff663756dee0a5e5024
Vignesh <vignesh@Vigneshs.local>
2014-08-19 19:27:48 +0530

    Initial Commit

---
+++ ScrollviewPager/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ScrollviewPager
+//
+//  Created by Vignesh on 8/19/14.
+//  Copyright (c) 2014 Vignesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ScrollviewPagerTests/ScrollviewPagerTests.m
@@ -0,0 +1,34 @@
+//
+//  ScrollviewPagerTests.m
+//  ScrollviewPagerTests
+//
+//  Created by Vignesh on 8/19/14.
+//  Copyright (c) 2014 Vignesh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ScrollviewPagerTests : XCTestCase
+
+@end
+
+@implementation ScrollviewPagerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
